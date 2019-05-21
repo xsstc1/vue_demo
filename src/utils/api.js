bb3a65f8c3d9037b8aa055afc8ff89b51153be80
xsstc1 <xsstc1@163.com>
2019-05-21 17:48:27 +0800

    fix bug

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import {
   get
 } from './axios.js';
-export function getKeyWords(type) {
+export function getKeyWords() {
   return get(`https://www.easy-mock.com/mock/597ae24fa1d30433d8408d2c/icms/keywords`);
 }
